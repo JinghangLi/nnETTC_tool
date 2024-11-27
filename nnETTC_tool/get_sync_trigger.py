378b43953dec1e397faeac7351e63ca646c80ba9
Jinghangli <jhanglee.ro@gmail.com>
2024-11-27 15:49:42 +0800

    modify the sync trigger code

--- nnETTC_tool/get_sync_trigger.py
+++ nnETTC_tool/get_sync_trigger.py
@@ -15,7 +15,7 @@ def rostime_to_ts(rostime):
 def get_rgb_trigger_timemap(rosbag_path: str, 
                             rgb_topic: str, 
                             exp_topic: str, 
-                            rgb_freq: int) -> pd.DataFrame:
+                            rgb_expect_freq: int) -> pd.DataFrame:
     """
                      <--- exposure_ts ---> exp_header_ts
                      <--- exposure_ts ---> rgb_header_ts
@@ -23,7 +23,7 @@ def get_rgb_trigger_timemap(rosbag_path: str,
     """
     rgb_trigger_list = []
     last_trigger_ts = 0
-    expected_interval = 1.0 / int(rgb_freq)
+    expect_time_interval = 1.0 / int(rgb_expect_freq)
     bool_first_frame = True
 
     with rosbag.Bag(rosbag_path, 'r') as bag:
@@ -32,14 +32,14 @@ def get_rgb_trigger_timemap(rosbag_path: str,
 
         for exp_cnt, (_, exp_msg, _) in enumerate(bag.read_messages(topics=[exp_topic])):
             rgb_exp_end_ts = rostime_to_ts(exp_msg.header.stamp)
-            exposure_ts = exp_msg.exposure_time / float(1e6) # exp_msg.exposure_time unit is us, convert to s
+            exposure_ts = exp_msg.exposure_time / float(1e6)   # exp_msg.exposure_time unit is us, convert to s
             rgb_exp_start_ts = rgb_exp_end_ts - exposure_ts
             dt = rgb_exp_start_ts - last_trigger_ts
             last_trigger_ts = rgb_exp_start_ts
 
-            # Judge if dt is bigger than 50% of expected_interval
-            if not bool_first_frame and abs(dt - expected_interval) > expected_interval * 0.2:
-                error_message = f"Error: {exp_cnt:010d}, RGB frequency is not [{int(rgb_freq)}], the time difference is [{dt}]"
+            # Judge if dt is bigger than 20% of expected_interval
+            if not bool_first_frame and abs(dt - expect_time_interval) > expect_time_interval * 0.2:
+                error_message = f"Error: {exp_cnt:010d}, RGB frequency is not [{int(rgb_expect_freq)}], the time difference is [{dt}]"
                 print(f"\033[91m{error_message}\033[0m")
                 continue
             if bool_first_frame:
@@ -75,7 +75,7 @@ def get_rgb_trigger_timemap(rosbag_path: str,
 
 def get_event_trigger_timemap(rosbag_path, event_topic=None):
     """
-    |=== e_msg_header: e_msg_ts (computer clock) ====
+    |=== e_msg_header: e_msg_header_ts (computer clock) ====
     |
     |--- event 1 ----
     |--- event 2 ----
@@ -92,10 +92,10 @@ def get_event_trigger_timemap(rosbag_path, event_topic=None):
             trig_events = decoder.get_ext_trig_events()  # get single trigger event in one event message
 
             if trig_events.size != 0 and trig_events[0][0] == 0:
-                e_msg_ts = rostime_to_ts(e_msg.header.stamp)
+                e_msg_header_ts = rostime_to_ts(e_msg.header.stamp)
                 event_trig_list.append({
                     'event_index': f"{e_cnt:010d}",
-                    'e_msg_ts': e_msg_ts,
+                    'e_msg_header_ts': e_msg_header_ts,
                     'event_trigger_tnsec': trig_events.tolist()[0][1]*1000  # nanosecond
                 })
 
@@ -107,49 +107,35 @@ def get_event_trigger_timemap(rosbag_path, event_topic=None):
 
 def match_triggers(rgb_trigger_df: pd.DataFrame, 
                    event_trigger_df: pd.DataFrame, 
-                   threshold_ts=0.02,
-                   zyt_hybrid_diff_ts=None) -> pd.DataFrame:
+                   threshold_tms=15) -> pd.DataFrame:
     """
-    |=== e_msg_header: e_msg_ts (computer clock) ====
-    |
-    |--- event 1 ----
-    |--- event 2 ----
-    |--- trig_events: trig_events_ts (event camera clock) ----
-    |--- event 4 ----
+    Match the event triggers to the rgb triggers
     """
-    for _, e_msg_row in tqdm(event_trigger_df.iterrows(), total=len(event_trigger_df), desc="Matching triggers"):
-        e_msg_ts = e_msg_row['e_msg_ts']
+    for _, e_trigger_row in tqdm(event_trigger_df.iterrows(), total=len(event_trigger_df), desc="Matching triggers"):
+        e_msg_header_ts = e_trigger_row['e_msg_header_ts']
         # Find the corresponding row in rgb_trigger_df
-        matching_row = rgb_trigger_df[(rgb_trigger_df['rgb_exp_start_ts'] - e_msg_ts).abs() < threshold_ts]
+        threshold_ts = threshold_tms / 1000
+        matching_row = rgb_trigger_df[(rgb_trigger_df['rgb_exp_start_ts'] - e_msg_header_ts).abs() < threshold_ts]
         
         if not matching_row.empty and len(matching_row) == 1:
-            rgb_trigger_df.loc[matching_row.index, 'e_msg_ts'] = e_msg_ts   # second
-            rgb_trigger_df.loc[matching_row.index, 'event_trigger_tnsec'] = e_msg_row['event_trigger_tnsec']  # nanosecond
+            rgb_trigger_df.loc[matching_row.index, 'e_msg_header_ts'] = e_msg_header_ts   # second
+            rgb_trigger_df.loc[matching_row.index, 'event_trigger_tnsec'] = e_trigger_row['event_trigger_tnsec']  # nanosecond
         else:
             matching_ts_value = rgb_trigger_df.loc[
-                (rgb_trigger_df['rgb_exp_start_ts'] - e_msg_ts).abs().idxmin(),
+                (rgb_trigger_df['rgb_exp_start_ts'] - e_msg_header_ts).abs().idxmin(),
                 'rgb_exp_start_ts'
             ]
-            min_ts_diff = abs(e_msg_ts - matching_ts_value)
+            min_ts_diff = abs(e_msg_header_ts - matching_ts_value)
             error_message = (
                 f"Error: Matching EVENT trigger to RGB failed:\n "
-                f"e_msg_ts:    [{e_msg_ts}]\n "
-                f"matching_ts: [{matching_ts_value}]\n "
-                f"min_ts_diff: [{min_ts_diff}]"
+                f"e_msg_header_ts: [{e_msg_header_ts}]\n "
+                f"matching_ts:     [{matching_ts_value}]\n "
+                f"min_ts_diff:     [{min_ts_diff}]"
             )
             print(f"\033[91m{error_message}\033[0m")
             continue
     
     trigger_debug_df = rgb_trigger_df
-    
-    # Align the timestamps of the zyt sensor and the hybrid sensor
-    if zyt_hybrid_diff_ts is not None:
-        assert zyt_hybrid_diff_ts > 0, "Error: zyt_hybrid_diff_ts < 0"
-        rgb_trigger_df['rgb_exp_start_ts'] -= zyt_hybrid_diff_ts
-        rgb_trigger_df['rgb_exp_end_ts'] -= zyt_hybrid_diff_ts
-    
-    # calculate the time difference between the event trigger and the rgb trigger
-    rgb_trigger_df['t_offset'] = rgb_trigger_df['rgb_exp_start_ts'] * 1e6 - rgb_trigger_df['event_trigger_tnsec'] / 1e3  # microsecond
     
     # Select row as as input of calibration
     match_triggers_df = rgb_trigger_df[
@@ -177,7 +163,7 @@ if __name__ == "__main__":
     rgb_trigger_df = get_rgb_trigger_timemap(rosbag_path=args.rosbag_path,
                                             rgb_topic=args.rgb_topic,
                                             exp_topic=args.exp_topic,
-                                            rgb_freq=args.rgb_freq)
+                                            rgb_expect_freq=args.rgb_freq)
     rgb_trigger_df.to_csv(Path(args.timemap_path).parent / 'rgb_trigger_df.csv', sep=' ', header=True, index=False)
 
 
